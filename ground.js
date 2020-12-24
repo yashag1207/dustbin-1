b2c711b2776c5ef772ca5aa8f73fa41f5b78db47
yashag1207 <74239906+yashag1207@users.noreply.github.com>
2020-12-24 16:33:10 +0530

    Add files via upload

---
+++ ground.js
@@ -0,0 +1,33 @@
+class ground {
+   constructor(){
+var options={
+    isStatic: true 
+}
+this.x=x 
+this.y=y 
+this.w=w 
+this.h=h 
+this.body=Bodies.rectangle(x,y,w,h,options);
+World .add(world,this,boby);
+
+}
+
+display(){
+var pos=this.body.position;
+push()
+translate(pos.x,pos,y);
+rectmode(center);
+strokeWeight(4);
+Fill(255,255,0);
+rect(0,0,this,w,this.h);
+Pop()
+
+
+
+}
+
+   
+
+
+
+}
---
+++ paper.js
@@ -0,0 +1,34 @@
+class paper{
+
+constructor(x,y,r){
+
+var option={
+    isStatic:false,
+    restitution : 0.3,
+    friction : 0.5,
+    density :1.2
+}
+
+this.x=x
+this.y=y
+this.r=r
+this.body=Bodies.circle(this.x,this.y,this.r/2,option)
+
+World.add(world,this.body);
+
+}
+dispaly(){
+var pos=this.body.position;
+push()
+translate(pos.x,pos.y);
+rectMode(CENTER);
+strokeWeight(3);
+fill(255,0,255);
+ellipse(0,0,this.r,this.r);
+pop()
+
+
+}
+
+
+}
